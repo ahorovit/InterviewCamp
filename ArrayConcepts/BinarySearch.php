f95a57494edc73188dc38b560af50e4c91b75391
Adin Horovitz <adin@tapclicks.com>
2019-08-18 17:03:14 -0700

    cyclically sorted

--- ArrayConcepts/BinarySearch.php
+++ ArrayConcepts/BinarySearch.php
@@ -3,18 +3,20 @@
 
 namespace InterviewCamp\ArrayConcepts;
 
-$input = [1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 5, 6, 7, 8];
+$input = [9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8];
 $target = 3;
 
 $search = new BinarySearch();
 
-$result = $search->findFirstMatch($input, $target);
+$result = $search->findMinOfCyclicSortedArray($input);
 
-if ($result >= 0) {
-    print "\nTarget found at index: {$result}\n";
-} else {
-    print "\nTarget not found in array\n";
-}
+print "Minimum element is at index: {$result}\n";
+
+//if ($result >= 0) {
+//    print "\nTarget found at index: {$result}\n";
+//} else {
+//    print "\nTarget not found in array\n";
+//}
 
 class BinarySearch
 {
@@ -71,6 +73,31 @@ class BinarySearch
 
         // $target not found
         return -1;
+    }
+
+    public function findMinOfCyclicSortedArray(array $values) : int
+    {
+        $start = 0;
+        $end = count($values) - 1;
+
+        // Rightmost value is special:
+        // All elements to the left of min will be greater than rightmost
+        // All elements to the right of min will be less than rightmost
+        $rightmost_value = $values[$end];
+
+        while ($start <= $end) {
+            $mid_index = $this->getMidpointIndex($start, $end);
+            $middle_value = $values[$mid_index];
+
+            // If we've reached 0th index, array has not been cycled, and it's just a normal sorted array
+            if ($mid_index == 0 || $values[$mid_index - 1] > $middle_value) {
+                return $mid_index;
+            }elseif ($middle_value > $rightmost_value) {
+                $start = $mid_index + 1;
+            } else{
+                $end = $mid_index - 1;
+            }
+        }
     }
 
 }
